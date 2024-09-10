4556dc334035ff6c7ca26c341324d0f06f61ac5e
Pedro V. <157400039+Pixel-DefaultBR@users.noreply.github.com>
2024-09-10 11:47:42 -0300

    Delete helpers directory

--- helpers/sanitizer.php
+++
@@ -1,8 +0,0 @@
-<?php
-function userInputSantizer($userInput)
-{
-    $userInput = preg_replace('#\'#', '&apos;', $userInput);
-    $userInput = preg_replace('#\\\#', '', $userInput);
-    $userInput = htmlspecialchars($userInput);
-    return $userInput;
-}
--- helpers/url.php
+++
@@ -1,2 +0,0 @@
-<?php
- $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . "/Blog". "/";
